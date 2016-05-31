05dbdbf09fcc3e1378c31fdf5dffae76513adbcc
Nechaev Vasiliy Anatolyevich <v.a.nechaev@tcsbank.ru>
2016-05-31 18:25:20 +0300

    multiplier added

--- src/main/scala/ru/osfb/trading/cases/BalancingStrategyTest.scala
+++ src/main/scala/ru/osfb/trading/cases/BalancingStrategyTest.scala
@@ -27,14 +27,14 @@ object BalancingStrategyTest extends App with LazyLogging {
   val fetchTimeStart = from minusSeconds 86400*365 // minusSeconds 5*timeFrame
 
   val trades =
-  Await.result(CsvHistoryStore.loadHistory(args(0), fetchTimeStart, till), 1.hour)
-  //BfxData.loadVwapData("BTCUSD", fetchTimeStart.toEpochMilli / 1000, till.toEpochMilli / 1000)
+  //Await.result(CsvHistoryStore.loadHistory(args(0), fetchTimeStart, till), 1.hour)
+  BfxData.loadVwapData("BTCUSD", fetchTimeStart.toEpochMilli / 1000, till.toEpochMilli / 1000)
   //  Finam.loadCsvTrades(args(0))
   implicit val history = new ArrayTradeHistory(trades)
 
   logger.info(s"Running backtest from $from till $till")
 
-  def run(timeFrame: Long, hysteresis: Double) = {
+  def run(timeFrame: Long, hysteresis: Double, multiplier: Double) = {
     val startPrice = history.priceAt(from.getEpochSecond) //EMA(from.getEpochSecond, timeStep)
     val (baseBalance, instrumentBalance, turnover) = (from.getEpochSecond to till.getEpochSecond by timeStep)
         .foldLeft((50.0, 50.0 / startPrice, 0.0))((balance, time) => {
@@ -42,14 +42,20 @@ object BalancingStrategyTest extends App with LazyLogging {
           val tf = CovarianceTrendFactor(time, timeFrame)
           val price = history.priceAt(time) //EMA(time, timeStep)
           val totalBalance = instrumentBalance * price + baseBalance
-          val diff = tf - ((instrumentBalance * price - baseBalance) / totalBalance)
-          if (Math.abs(diff) > hysteresis) {
-            val tradeAmount = totalBalance * diff / 1.1
+          val currentRatio = (instrumentBalance * price - baseBalance) / totalBalance
+          val diff = tf*multiplier - currentRatio
+          if (Math.abs(diff) > hysteresis && currentRatio > -0.9 && currentRatio < 0.9) {
+            val adjustedDiff = diff match {
+              case d if d + currentRatio > 1.0 => 1.0 - currentRatio
+              case d if d + currentRatio < -1.0 => -1.0 - currentRatio
+              case d => d * 0.99
+            }
+            val tradeAmount = totalBalance * adjustedDiff
             (baseBalance - tradeAmount, instrumentBalance + (tradeAmount / price), turnover + Math.abs(tradeAmount))
           } else balance
         })
     val endPrice = history.priceAt(till.getEpochSecond) //EMA(till.getEpochSecond, timeStep)
-    (baseBalance + instrumentBalance*endPrice, 100*startPrice/endPrice, turnover)
+    (baseBalance + instrumentBalance*endPrice, 100*endPrice/startPrice, turnover)
   }
 
 //  val results = for (timeFrame <- (50000 to 500000 by 5000).par) yield (timeFrame, run(timeFrame, defaultHysteresis))
@@ -60,8 +66,11 @@ object BalancingStrategyTest extends App with LazyLogging {
 //  val (hysteresis, (newBalance, reference, turnover)) = results.reduce((r1,r2) => if (r1._2._1 > r2._2._1) r1 else r2)
 //  logger.info(s"hysteresis: $hysteresis, max balance: $newBalance, reference: $reference, turnover: $turnover")
 
-  val (newBalance, reference, turnover) = run(defaultTimeFrame, defaultHysteresis)
+    val results = for (multiplier <- (0.1 to 5.0 by 0.01).par) yield (multiplier, run(defaultTimeFrame, defaultHysteresis, multiplier))
+    val (multiplier, (newBalance, reference, turnover)) = results.reduce((r1,r2) => if (r1._2._1 > r2._2._1) r1 else r2)
+    logger.info(s"multiplier: $multiplier, max balance: $newBalance, reference: $reference, turnover: $turnover")
 
-  logger.info(s"balance: $newBalance, reference: $reference, turnover: $turnover")
+//  val (newBalance, reference, turnover) = run(defaultTimeFrame, defaultHysteresis, 2.0)
+//  logger.info(s"balance: $newBalance, reference: $reference, turnover: $turnover")
 
 }
